b73a5a6cea35dce21d6b599ba8634ed765a08eed
zeltiaferver <fvereszeltia@gmail.com>
2025-05-13 16:29:14 +0200

    funciones

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * funcion recoger datos 
+ */
+function entradaDAtos(){
+    const primerOcteto = document.getElementById("primerOcteto");
+    const segundoOcteto = document.getElementById("segundoOcteto");
+    const tercerOcteto = document.getElementById("tercerOcteto");
+    const cuartoOcteto = document.getElementById("cuartoOcteto");
+}
+/** 
+ * funcionalidad botón
+ */
+const botonCalc= document.getElementById("boton1");
+
+botonCalc.addEventListener("click", () => {
+    //llamar funciones y mostrar info
+});
