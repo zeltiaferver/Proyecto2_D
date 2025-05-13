872797b30519d80faa3959da8ef5d6417aa37ba6
zeltiaferver <fvereszeltia@gmail.com>
2025-05-13 17:12:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /**
- * funcion recoger datos 
+ * constantes
  */
-function entradaDAtos(){
     const primerOcteto = document.getElementById("primerOcteto");
     const segundoOcteto = document.getElementById("segundoOcteto");
     const tercerOcteto = document.getElementById("tercerOcteto");
     const cuartoOcteto = document.getElementById("cuartoOcteto");
-}
+    var correcto= false;
+    
 /** 
  * funcionalidad botón
  */
@@ -14,4 +14,23 @@ const botonCalc= document.getElementById("boton1");
 
 botonCalc.addEventListener("click", () => {
     //llamar funciones y mostrar info
+    validaciones();
+
 });
+
+
+/**
+ * funcion validaciones
+ */
+function validaciones(){
+    const pattern = /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$/;
+
+    if (primerOcteto==""|| segundoOcteto==""||tercerOcteto==""||cuartoOcteto==""){
+        alert("Rellena todos los campos.");
+    }else if(primerOcteto==pattern|| segundoOcteto==pattern||tercerOcteto==pattern||cuartoOcteto==pattern){
+        return correcto=true;
+    }else{
+        alert("Rellena con valores válidos.")
+    }
+
+}
